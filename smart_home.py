5ec5f45600409577b8f3275bd39b7b48d125441b
Dora Krstulovic <dora.krstulovic@outlook.com>
2022-06-30 21:28:00 +0200

    novo

--- smart_home.py
+++ smart_home.py
@@ -18,39 +18,44 @@ root.title('Vrata')
 status=[]
 
 def key():
-    rezultat_kuhinja.configure()
+    rezultat_ulazna.configure("Zaključano")
+    rezultat_kuhinja.configure("Zaključano")
+    rezultat_kupaona.configure("Zaključano")
+    rezultat_spavaca.configure("Zaključano")
+    rezultat_dnevna.configure("Zaključano")
+    rezultat_balkon.configure("Zaključano")
 
      
 
 frame_vrata= tk.Frame(root,width=100,height=200, borderwidth=2, relief=tk.RIDGE)
 frame_vrata.grid(row=0,column=0,padx=10,pady=10)
 
-button_ulazna= tk.Button(frame_vrata, text="Ulazna",fg="purple")#funkcija!!!
+button_ulazna= tk.Button(frame_vrata, text="Ulazna",fg="purple",command=key)#funkcija!!!
 button_ulazna.grid(column=0,row=1,padx=10,pady=10)
 rezultat_ulazna= tk.Label(frame_vrata,text="Otključano")
 rezultat_ulazna.grid(column=1,row=1,padx=10,pady=10)
 
-button_kuhinja= tk.Button(frame_vrata, text="Kuhinja",fg="purple")#funkcija!!!
+button_kuhinja= tk.Button(frame_vrata, text="Kuhinja",fg="purple",command=key)#funkcija!!!
 button_kuhinja.grid(column=0,row=2,padx=10,pady=10)
 rezultat_kuhinja= tk.Label(frame_vrata,text="Otključano")
 rezultat_kuhinja.grid(column=1,row=2,padx=10,pady=10)
 
-button_kupaona= tk.Button(frame_vrata, text="Kupaonica",fg="purple")#funkcija!!!
+button_kupaona= tk.Button(frame_vrata, text="Kupaonica",fg="purple",command=key)#funkcija!!!
 button_kupaona.grid(column=0,row=3,padx=10,pady=10)
 rezultat_kupaona= tk.Label(frame_vrata,text="Otključano")
 rezultat_kupaona.grid(column=1,row=3,padx=10,pady=10)
 
-button_spavaca= tk.Button(frame_vrata, text="Spavaća soba",fg="purple")#funkcija!!!
+button_spavaca= tk.Button(frame_vrata, text="Spavaća soba",fg="purple",command=key)#funkcija!!!
 button_spavaca.grid(column=0,row=4,padx=10,pady=10)
 rezultat_spavaca= tk.Label(frame_vrata,text="Otključano")
 rezultat_spavaca.grid(column=1,row=4,padx=10,pady=10)
 
-button_dnevna= tk.Button(frame_vrata, text="Dnevna soba",fg="purple")#funkcija!!!
+button_dnevna= tk.Button(frame_vrata, text="Dnevna soba",fg="purple",command=key)#funkcija!!!
 button_dnevna.grid(column=0,row=5,padx=10,pady=10)
 rezultat_dnevna= tk.Label(frame_vrata,text="Otključano")
 rezultat_dnevna.grid(column=1,row=5,padx=10,pady=10)
 
-button_balkon= tk.Button(frame_vrata, text="Balkon",fg="purple")#funkcija!!!
+button_balkon= tk.Button(frame_vrata, text="Balkon",fg="purple",command=key)#funkcija!!!
 button_balkon.grid(column=0,row=6,padx=10,pady=10)
 rezultat_balkon= tk.Label(frame_vrata,text="Otključano")
 rezultat_balkon.grid(column=1,row=6,padx=10,pady=10)
